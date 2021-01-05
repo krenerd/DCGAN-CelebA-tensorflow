eb50ec2605585cf844a895869cebb7a78b874dbd
krenerd <48239275+krenerd@users.noreply.github.com>
2021-01-05 11:58:07 +0900

    Update model.py

--- model.py
+++ model.py
@@ -3,55 +3,56 @@ import tensorflow as tf
 
 def build_generator(image_size=(64,64),channels=64):
     w,h=image_size
+    init=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
     
     model = tf.keras.Sequential()
-    model.add(layers.Dense(8*8*channels, use_bias=False, input_shape=(100,)))
+    model.add(layers.Dense(8*8*channels, use_bias=False, input_shape=(100,),kernel_initializer=init))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
     model.add(layers.Reshape((8,8,channels*4)))
 
-    model.add(layers.Conv2DTranspose(channels*4, 4, strides=(1, 1), padding='same', use_bias=False))
+    model.add(layers.Conv2DTranspose(channels*4, 4, strides=(1, 1), padding='same', use_bias=False,kernel_initializer=init))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(channels*2, 4, strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.Conv2DTranspose(channels*2, 4, strides=(2, 2), padding='same', use_bias=False,kernel_initializer=init))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(channels, 4, strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.Conv2DTranspose(channels, 4, strides=(2, 2), padding='same', use_bias=False,kernel_initializer=init))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
     
-    model.add(layers.Conv2DTranspose(3, 4, strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    model.add(layers.Conv2DTranspose(3, 4, strides=(2, 2), padding='same', use_bias=False,kernel_initializer=init, activation='tanh'))
     model.add(layers.experimental.preprocessing.Resizing(w,h))
 
     return model
 
 def build_discriminator(image_size=(64,64),channels=64):
     model = tf.keras.Sequential()
+    init=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
 
-    #model.add(layers.experimental.preprocessing.Resizing(64,64))
-    model.add(layers.Conv2D(channels, 4, strides=(2, 2), padding='same',input_shape=(64,64,3))
+    model.add(layers.Conv2D(channels, 4, strides=(2, 2), padding='same',input_shape=(64,64,3),kernel_initializer=init))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
     #model.add(layers.Dropout(0.3))
 
-    model.add(layers.Conv2D(channels*2, 4, strides=(2, 2), padding='same'))
+    model.add(layers.Conv2D(channels*2, 4, strides=(2, 2), padding='same',kernel_initializer=init))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
     #model.add(layers.Dropout(0.3))
 
-    model.add(layers.Conv2D(channels*4, 4, strides=(2, 2), padding='same'))
+    model.add(layers.Conv2D(channels*4, 4, strides=(2, 2), padding='same',kernel_initializer=init))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2D(channels*8, 4, strides=(2, 2), padding='same'))
+    model.add(layers.Conv2D(channels*8, 4, strides=(2, 2), padding='same',kernel_initializer=init))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
     model.add(layers.Flatten())
-    model.add(layers.Dense(1,activation='sigmoid'))
+    model.add(layers.Dense(1,activation='sigmoid',kernel_initializer=init))
 
     return model
 
